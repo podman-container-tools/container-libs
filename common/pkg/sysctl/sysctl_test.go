e51ef3d579704600e27627dcf76c9b9bc4949858
Ijaj Mulani <ijajmulani786@gmail.com>
2026-09-18 07:35:15 -0500

    common/pkg/sysctl: accept slash-separated sysctl names Normalize sysctl keys per sysctl.d(5) before validation so slash-first names like net/ipv6/conf/bond1.340/autoconf are accepted for VLAN interface names containing dots.
    Signed-off-by: Ijaj Mulani <ijajmulani786@gmail.com>

--- common/pkg/sysctl/sysctl_test.go
+++ common/pkg/sysctl/sysctl_test.go
@@ -19,3 +19,31 @@ func TestValidateBadSysctl(t *testing.T) {
 	_, err := Validate(strSlice)
 	assert.Error(t, err)
 }
+
+func TestConvertSysctlVariableToDotsSeparator(t *testing.T) {
+	cases := []struct {
+		in  string
+		out string
+	}{
+		{in: "kernel.shm_rmid_forced", out: "kernel.shm_rmid_forced"},
+		{in: "kernel/shm_rmid_forced", out: "kernel.shm_rmid_forced"},
+		{in: "net.ipv4.conf.eno2/100.rp_filter", out: "net.ipv4.conf.eno2/100.rp_filter"},
+		{in: "net/ipv4/conf/eno2.100/rp_filter", out: "net.ipv4.conf.eno2/100.rp_filter"},
+		{in: "net/ipv6/conf/bond1.340/autoconf", out: "net.ipv6.conf.bond1/340.autoconf"},
+	}
+
+	for _, tc := range cases {
+		assert.Equal(t, tc.out, convertSysctlVariableToDotsSeparator(tc.in))
+	}
+}
+
+func TestValidateSlashSeparatedSysctl(t *testing.T) {
+	strSlice := []string{
+		"net/ipv6/conf/bond1.340/autoconf=0",
+		"net.ipv6.conf.bond1/340.autoconf=0",
+	}
+	result, err := Validate(strSlice)
+	require.Nil(t, err)
+	assert.Equal(t, "0", result["net/ipv6/conf/bond1.340/autoconf"])
+	assert.Equal(t, "0", result["net.ipv6.conf.bond1/340.autoconf"])
+}
